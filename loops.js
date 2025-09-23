937aca05948b15bc916c1d9e49b3a833eda0066c
NAVEENKUMARPATNANA-123 <naveenkumar.patnana@softsuave.org>
2025-09-23 17:45:47 +0530

    loops and control structures

---
+++ loops.js
@@ -0,0 +1,14 @@
+// let products = [
+//   { id: 1, name: "Laptop", price: 1200 },
+//   { id: 2, name: "Phone", price: 800 },
+//   { id: 3, name: "Headphones", price: 150 }
+// ];
+
+// for (let product of products) {
+//   console.log(`${product.id}: ${product.name} - ${product.price}`);
+// }
+
+let a={1:'a',2:'b',3:'c'}
+for ( let key in a){
+    console.log(`${key}: ${a[key]}`);
+}
