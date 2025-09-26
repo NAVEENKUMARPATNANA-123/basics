e71a32c5b1a87e404bec4a0338f65ae362aed9ac
NAVEENKUMARPATNANA-123 <naveenkumar.patnana@softsuave.org>
2025-09-26 11:35:23 +0530

    promises

--- promise.js
+++ promise.js
@@ -1,13 +1,50 @@
-let burgerOrder = new Promise((resolve, reject) => {
-  let burgerReady = false; 
-  if (burgerReady) {
-    resolve("Here’s your food!");
-  } else {
-    reject("Sorry, we’re out of burgers ");
-  }
-});
-burgerOrder
-  .then(result => console.log(result))   // if success
-  .catch(error => console.log(error))   // if fail
-  .finally(() => console.log("Order finished "));
+const arr=["apple","banana","grapes"];
+const ar=validate(arr);
+ar.then (function (ord){
+    console.log("Success");
+    return ord;
+})
+.then((ord)=> {
+    console.log("Success");
+    console.log(ord);
+})
+.catch(function (err){
+    console.log("Error occurred",err);
+})
+function validate( arr){
+    const a=new Promise(function (resolve,reject){
+        const request=false;
+        if (!request){
+            const q=new Error("Sorry");
+            reject(q);
+        }
+        else{
+            resolve("Validated 1234");
+        }
+        
+    })
+    return a;
 
+}
+
+// const arr = ["apple", "banana", "grapes"];
+
+// const ar = validate(arr);
+
+// ar.then(function (ord) {
+//     console.log("Success");
+// })
+// .catch(function (err) {
+//     console.log("Error occurred:", err.message); // show error message
+// });
+
+// function validate(arr) {
+//     return new Promise(function (resolve, reject) {
+//         // Example: reject with an error
+//         const q = new Error("Sorry");
+//         reject(q);
+
+//         // OR: resolve if valid
+//         // resolve("Array is valid!");
+//     });
+// }
